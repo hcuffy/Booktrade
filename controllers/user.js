93e441aa586c22b4460e513ec038ebbce5ffd6aa
Henry Cuffy <hcuffy_5@hotmail.com>
2018-05-01 15:56:09 +0200

    Add login & register files

--- controllers/user.js
+++ controllers/user.js
@@ -1,5 +1,10 @@
 exports.getProfile = (req, res, next) => {
-
     res.render('profile')
+
+}
+
+
+exports.getRegistration = (req, res, next) => {
+    res.render('register')
 
 }
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const searchController = require('../controllers/search');
-
+const userController = require('../controllers/user');
 
 router.use('/book', require('./books'));
 router.use('/user', require('./users'));
 
 router.get('/search', searchController.getSearch);
+router.get('/register', userController.getRegistration);
 router.get('/', (req, res) => {
   res.render('index');
 });
