33bc965426c2201d383ad84df234956e55f82595
Henry Cuffy <hcuffy_5@hotmail.com>
2018-06-23 17:39:34 +0200

    Update folder structure

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-const path = require ('path');
+const path = require('path');
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 const passport = require('passport');
 const session = require('express-session');
-
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/booktrade');
 
@@ -13,17 +13,19 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json('*/*'));
 
-app.use(session({secret: 'bookingtrading', saveUninitialized: true, resave: true}));
+app.use(
+	session({ secret: 'bookingtrading', saveUninitialized: true, resave: true })
+);
 app.use(passport.initialize());
 app.use(passport.session());
 
 app.use('*', function(req, res, next) {
-  res.locals.user = req.user || null;
-  res.locals.error = null;
-  next();
+	res.locals.user = req.user || null;
+	res.locals.error = null;
+	next();
 });
 
 const routes = require('./routes');
@@ -32,6 +34,5 @@ app.use('/', routes);
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-console.log('Express server has started on', port);
-
+	console.log('Express server has started on', port);
 });
